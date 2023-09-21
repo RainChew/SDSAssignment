704d85621a18f7af024046704c98ca894eade606
ChewLipSin <chewls-wm21@student.tarc.edu.my>
2023-09-21 16:43:50 +0800

    auto.arima

--- SDSAssignment/External_Trade_R.R
+++ SDSAssignment/External_Trade_R.R
@@ -1,18 +1,5 @@
-data <- read.csv("External_Trade_Data.csv")
-print(data)
 library(forecast)
 library(ggplot2)
-# data$Exports <- as.numeric(gsub(",", "", data$Exports))
-# data$Imports <- as.numeric(gsub(",", "", data$Imports))
-# data$Total_Trade <- as.numeric(gsub(",", "", data$Total_Trade))
-# data$Balance_of_Trade <- as.numeric(gsub(",", "", data$Balance_of_Trade))
-# #Drop Variable
-# data_ts = subset(data, select = -c(Imports,Total_Trade,Balance_of_Trade,Year))
-# data
-# data <- ts(data, frequency = 1, start =c(1967,1))
-# 
-# data <- log(data)
-# plot.ts(data)
 
 df <- read.csv("External_Trade_Monthly.csv")
 print(df)
@@ -24,4 +11,11 @@ df
 df <- ts(df, frequency = 12, start =c(2010,1))
 # df <- log(df)
 plot.ts(df)
+#Drop Variab1le
+df2 <- df[,5]
+plot.ts(df2)
 
+library(forecast)
+fit <- auto.arima(df)
+summary(fit)
+auto.arima(df, ic="aic", trace=TRUE)
