d9797f086d32209f5a5644a0cd232b7b6ba3f83c
ChewLipSin <chewls-wm21@student.tarc.edu.my>
2023-09-21 18:54:31 +0800

    Update External_Trade_R.R

--- SDSAssignment/External_Trade_R.R
+++ SDSAssignment/External_Trade_R.R
@@ -8,14 +8,15 @@ df$Gross_Imports <- as.numeric(gsub(",", "", df$Gross_Imports))
 df$Total_Trade <- as.numeric(gsub(",", "", df$Total_Trade))
 df$Trade_Balance <- as.numeric(gsub(",", "", df$Trade_Balance))
 df
-df <- ts(df, frequency = 12, start =c(2010,1))
-# df <- log(df)
+df <- ts(df, frequency = 12, start =c(1993,1))
+df <- log(df)
 plot.ts(df)
 #Drop Variab1le
-df2 <- df[,5]
+df2 <- df[,2]
 plot.ts(df2)
 
 library(forecast)
-fit <- auto.arima(df)
+fit <- auto.arima(df2)
 summary(fit)
-auto.arima(df, ic="aic", trace=TRUE)
+auto.arima(df2, ic="aic", trace=TRUE)
+
