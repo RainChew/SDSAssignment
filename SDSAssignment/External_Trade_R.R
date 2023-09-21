9d55a3e9d905a7f739560b9b8fab9bf6cb4f873e
ChewLipSin <chewls-wm21@student.tarc.edu.my>
2023-09-21 22:50:14 +0800

    rubbish

--- SDSAssignment/External_Trade_R.R
+++ SDSAssignment/External_Trade_R.R
@@ -8,11 +8,11 @@ df$Gross_Imports <- as.numeric(gsub(",", "", df$Gross_Imports))
 df$Total_Trade <- as.numeric(gsub(",", "", df$Total_Trade))
 df$Trade_Balance <- as.numeric(gsub(",", "", df$Trade_Balance))
 df
-df <- ts(df, frequency = 12, start =c(1993,1))
-df <- log(df)
+df <- ts(df, frequency = 12, start =c(2010,1))
+# df <- log(df)
 plot.ts(df)
 #Drop Variab1le
-df2 <- df[,2]
+df2 <- df[,5]
 plot.ts(df2)
 
 library(forecast)
